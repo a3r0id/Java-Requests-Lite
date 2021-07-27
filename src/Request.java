86f15c401576af7a4800f80f84f00d881837481b
hostinfodev <50676042+hostinfodev@users.noreply.github.com>
2021-07-27 12:51:59 -0700

    Slimmed Imports

--- src/Request.java
+++ src/Request.java
@@ -1,4 +1,7 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
